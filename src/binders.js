4b9e83a4501a17ef37472b1da464b4994fca1142
isar <i5ar@live.it>
2017-10-17 19:18:06 +0200

    Add binder requests
    - Create binder;
    - Retrieve binder;
    - Update binder;
    - Delete binder.

--- src/binders.js
+++ src/binders.js
@@ -2,6 +2,7 @@ import {json} from 'aurelia-fetch-client';
 import {inject} from 'aurelia-framework';
 import {State} from 'state';
 
+
 @inject(State)
 export class Binders {
   constructor(state) {
@@ -11,6 +12,23 @@ export class Binders {
     this.title;
     this.customer;
     this.containerId;
+  }
+
+  /**
+   * Get binders.
+   * {@link http://aurelia.io/hub.html#/doc/article/aurelia/fetch-client/latest/http-services/}
+   */
+  getBinders() {
+    this.state.http.fetch('api/shelves/binders/', {
+      headers: {
+        'Authorization': 'JWT ' + this.state.token
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data);
+      this.binders = data;
+    });
   }
 
   /**
@@ -30,9 +48,7 @@ export class Binders {
     };
     _state.http.fetch('api/shelves/binders/', {
       headers: {
-        'Authorization': 'JWT ' + _state.token,
-        'Accept': 'application/json',
-        'X-Requested-With': 'Fetch'
+        'Authorization': 'JWT ' + _state.token
       },
       method: 'post',
       body: json(binderForm)
@@ -48,22 +64,82 @@ export class Binders {
   }
 
   /**
-   * Get binders.
-   * {@link http://aurelia.io/hub.html#/doc/article/aurelia/fetch-client/latest/http-services/}
+   * Get binder from a form.
+   * If we want to reuse the method we must pass the form fields as arguments.
+   * @param {String} _state The state
+   * @param {String} _binderId The container id field
    */
-  getBinders() {
-    this.state.http.fetch('api/shelves/binders/', {
+  getBinder(_state, _binderId) {
+    _state.http.fetch('api/shelves/binders/' + _binderId + '/?format=json', {
       headers: {
-        'Authorization': 'JWT ' + this.state.token,
-        'Accept': 'application/json',
-        'X-Requested-With': 'Fetch'
-      }
+        'Authorization': 'JWT ' + _state.token
+      },
+      method: 'get'
     })
     .then(response => response.json())
     .then(data => {
       // console.log(data);
-      this.binders = data;
+      this.binder = data;
+    })
+    .catch(error => {
+      alert('Binder error!');
     });
   }
+
+  /**
+   * Put binder from a form.
+   * If we want to reuse the method we must pass the form fields as arguments.
+   * @param {String} _state The state
+   * @param {String} _title The title field
+   * @param {String} _customer The customer field
+   * @param {String} _containerId The container id field
+   * @param {String} _binderId The container id field
+   */
+   putBinder(_state, _title, _customer, _containerId, _binderId) {
+     let binderForm = {
+       title: _title,
+       customer: _customer,
+       container_id: _containerId
+     };
+     _state.http.fetch('api/shelves/binders/' + _binderId + '/?format=json', {
+       headers: {
+         'Authorization': 'JWT ' + _state.token
+       },
+       method: 'put',
+       body: json(binderForm)
+     })
+     .then(response => response.json())
+     .then(data => {
+       // console.log(data);
+       alert('Binder updated!');
+     })
+     .catch(error => {
+       alert('Binder error!');
+     });
+   }
+
+   /**
+    * Delete binder from a form.
+    * If we want to reuse the method we must pass the form fields as arguments.
+    * @param {String} _state The state
+    * @param {String} _binderId The container id field
+    */
+   deleteBinder(_state, _binderId) {
+     _state.http.fetch('api/shelves/binders/' + _binderId + '/?format=json', {
+       headers: {
+         'Authorization': 'JWT ' + _state.token
+       },
+       method: 'delete'
+     })
+     .then(data => {
+       // console.log(data);
+       if (data.ok) {
+         alert('Binder deleted!');
+       }
+     })
+     .catch(error => {
+       alert('Binder error!');
+     });
+   }
 
 }
